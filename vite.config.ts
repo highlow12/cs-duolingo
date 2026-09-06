e2c7a951f28fdf3a6f43bf9ee50c73ad416bfd7d
highlow12 <77542378+highlow12@users.noreply.github.com>
2026-09-06 19:23:59 +0900

    fix: type GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,9 @@ import adapter from "@sveltejs/adapter-static";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 
-const base = process.env.BASE_PATH ?? "";
+const base = (process.env.BASE_PATH ?? "") as "" | `/${string}`;
 
 export default defineConfig({
-  base,
   plugins: [
     sveltekit({
       compilerOptions: {
