ecb43e407d93d8396b530ac173c95dbcf7d613f8
highlow12 <77542378+highlow12@users.noreply.github.com>
2026-09-06 19:22:23 +0900

    ci: deploy app to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,21 @@ import adapter from "@sveltejs/adapter-static";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 
+const base = process.env.BASE_PATH ?? "";
+
 export default defineConfig({
+  base,
   plugins: [
     sveltekit({
       compilerOptions: {
         // Force runes mode for the project, except for libraries. Can be removed in svelte 6.
         runes: ({ filename }) =>
-          filename.split(/[/\\]/).includes("node_modules") ? undefined : true,
+          filename.split(/[/\\\\]/).includes("node_modules") ? undefined : true,
       },
       adapter: adapter({ fallback: "index.html" }),
+      paths: {
+        base,
+      },
       prerender: {
         handleUnseenRoutes: "ignore",
       },
